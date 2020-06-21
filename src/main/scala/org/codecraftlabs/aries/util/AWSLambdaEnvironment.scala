83e41d7991635d0bef44acf915d79ec3dce1a199
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-06-21 19:10:17 +0200

    more code

---
+++ src/main/scala/org/codecraftlabs/aries/util/AWSLambdaEnvironment.scala
@@ -0,0 +1,9 @@
+package org.codecraftlabs.aries.util
+
+object AWSLambdaEnvironment {
+  val FieldSeparator = "FIELD_SEPARATOR"
+  val FieldSeparatorDefaultValue = ","
+
+  val S3DestinationBucket = "S3_DESTINATION_BUCKET"
+  val S3DestinationBucketDefaultValue = "FakeBucket"
+}
