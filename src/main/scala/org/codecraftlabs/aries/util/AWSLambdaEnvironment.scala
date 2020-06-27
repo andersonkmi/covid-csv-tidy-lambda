3fc72c7f9f7ed9fb33491d019f900c19c27720c6
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-06-27 23:36:18 +0200

    Serverless config

--- src/main/scala/org/codecraftlabs/aries/util/AWSLambdaEnvironment.scala
+++ src/main/scala/org/codecraftlabs/aries/util/AWSLambdaEnvironment.scala
@@ -4,9 +4,6 @@ object AWSLambdaEnvironment {
   val FieldSeparator = "FIELD_SEPARATOR"
   val FieldSeparatorDefaultValue = ","
 
-  val S3DestinationBucket = "S3_DESTINATION_BUCKET"
-  val S3DestinationBucketDefaultValue = "FakeBucket"
-
   val RecordSQSUrl = "RECORD_SQS_URL"
   val RecordSQSUrlDefaultValue = "https://sqs.us-east-1.amazonaws.com/970221509170/csv-data-processor"
 }
