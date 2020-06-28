d9398c30edf73e654c82d2c21db52241ab6b2143
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-06-28 12:33:10 +0200

    Add state province

--- src/main/scala/org/codecraftlabs/aries/util/CovidRecord.scala
+++ src/main/scala/org/codecraftlabs/aries/util/CovidRecord.scala
@@ -1,3 +1,3 @@
 package org.codecraftlabs.aries.util
 
-case class CovidRecord(country: String, lastUpdate: String, confirmed: String, deaths: String, recovered: String)
+case class CovidRecord(country: String, stateProvince: String, lastUpdate: String, confirmed: String, deaths: String, recovered: String)
--- src/main/scala/org/codecraftlabs/aries/util/S3ObjectProcessor.scala
+++ src/main/scala/org/codecraftlabs/aries/util/S3ObjectProcessor.scala
@@ -14,6 +14,7 @@ object S3ObjectProcessor {
   private val logger: Logger = LogManager.getLogger(getClass)
   private val s3Service = AmazonS3ClientBuilder.standard.build
   private val CountryColumnNames = List("Country/Region", "Country_Region")
+  private val StateProvinceColumnNames = List("Province_State", "Province/State")
   private val LastUpdateColumnNames = List("Last Update", "Last_Update")
   private val ConfirmedColumnNames = List("Confirmed")
   private val DeathsColumnNames = List("Deaths")
@@ -29,6 +30,7 @@ object S3ObjectProcessor {
 
     var lineNumber: Int = 0
     var countryColPosition: Int = 0
+    var stateProvinceColPosition: Int = 0
     var lastUpdateColPosition: Int = 0
     var confirmedColPosition: Int = 0
     var deathsColPosition: Int = 0
@@ -44,12 +46,14 @@ object S3ObjectProcessor {
         val positions = getColumnPositions(tokens)
 
         countryColPosition = positions(CountryColumnNames)
+        stateProvinceColPosition = positions(StateProvinceColumnNames)
         lastUpdateColPosition = positions(LastUpdateColumnNames)
         confirmedColPosition = positions(ConfirmedColumnNames)
         deathsColPosition = positions(DeathsColumnNames)
         recoveredColPosition = positions(RecoveredColumnNames)
 
         if (countryColPosition <= 0 ||
+          stateProvinceColPosition <= 0 ||
           lastUpdateColPosition <= 0 ||
           confirmedColPosition <= 0 ||
           deathsColPosition <= 0 ||
@@ -61,12 +65,13 @@ object S3ObjectProcessor {
       } else {
         // Regular value processing
         val countryName = tokens(countryColPosition)
+        val stateProvince = tokens(stateProvinceColPosition)
         val lastUpdate = tokens(lastUpdateColPosition)
         val confirmed = tokens(confirmedColPosition)
         val deaths = tokens(deathsColPosition)
         val recovered = tokens(recoveredColPosition)
 
-        val record = CovidRecord(countryName, lastUpdate, confirmed, deaths, recovered)
+        val record = CovidRecord(countryName, stateProvince, lastUpdate, confirmed, deaths, recovered)
         // Join the fields
         processedLines.addOne(record)
       }
@@ -79,17 +84,20 @@ object S3ObjectProcessor {
 
   private def getColumnPositions(tokens: Array[String]): Map[List[String], Integer] = {
     val countryColPosition = tokens.indexWhere(CountryColumnNames.contains(_))
+    val stateProvinceColPosition = tokens.indexWhere(StateProvinceColumnNames.contains(_))
     val lastUpdateColPosition = tokens.indexWhere(LastUpdateColumnNames.contains(_))
     val confirmedColPosition = tokens.indexWhere(ConfirmedColumnNames.contains(_))
     val deathsColPosition = tokens.indexWhere(DeathsColumnNames.contains(_))
     val recoveredColPosition = tokens.indexWhere(RecoveredColumnNames.contains(_))
     logger.info(s"Country column position: $countryColPosition")
+    logger.info(s"State/Province column position: $stateProvinceColPosition")
     logger.info(s"Last update column position: $lastUpdateColPosition")
     logger.info(s"Confirmed column position: $confirmedColPosition")
     logger.info(s"Deaths column position: $deathsColPosition")
     logger.info(s"Recovered column position: $recoveredColPosition")
 
     Map(CountryColumnNames -> countryColPosition,
+      StateProvinceColumnNames -> stateProvinceColPosition,
       LastUpdateColumnNames -> lastUpdateColPosition,
       ConfirmedColumnNames -> confirmedColPosition,
       DeathsColumnNames -> deathsColPosition,
