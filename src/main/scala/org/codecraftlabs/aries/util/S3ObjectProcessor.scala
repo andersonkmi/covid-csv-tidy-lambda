40d8fb5b93b8a6ffc23909ed67e5408796b51e08
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-07-28 19:43:54 +0200

    more code

--- src/main/scala/org/codecraftlabs/aries/util/S3ObjectProcessor.scala
+++ src/main/scala/org/codecraftlabs/aries/util/S3ObjectProcessor.scala
@@ -21,8 +21,8 @@ object S3ObjectProcessor {
   private val DeathsColumnNames = List("Deaths")
   private val RecoveredColumnNames = List("Recovered")
   private val dateTimeFormatYYYY_MM_DD_HH_MM_SS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-  private val dateTimeFormatYYYY_MM_DD_T_HH_MM_SS = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ss")
-  private val dateTimeFormatMM_DD_YYYY_HHMM = new SimpleDateFormat("m/dd/yyyy HH:mm")
+  private val dateTimeFormatYYYY_MM_DD_T_HH_MM_SS = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+  private val dateTimeFormatMM_DD_YYYY_HHMM = new SimpleDateFormat("M/dd/yyyy HH:mm")
 
   def readObject(bucket: String, key: String): List[CovidRecord] = {
     logger.info("Starting S3 object processing")
