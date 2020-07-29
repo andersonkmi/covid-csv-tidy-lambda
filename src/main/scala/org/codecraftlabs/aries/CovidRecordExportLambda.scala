b2372cf6dfc9895eedc99bfd4c109b5a05ad90cd
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-07-29 16:17:48 +0200

    more code

--- src/main/scala/org/codecraftlabs/aries/CovidRecordExportLambda.scala
+++ src/main/scala/org/codecraftlabs/aries/CovidRecordExportLambda.scala
@@ -1,14 +1,19 @@
 package org.codecraftlabs.aries
 
+import java.text.SimpleDateFormat
+
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import org.apache.logging.log4j.{LogManager, Logger}
 import org.codecraftlabs.aries.util.AWSLambdaEnvironment.{DestinationS3Bucket, DestinationS3Prefix, NumberIterations}
-import org.codecraftlabs.aries.util.{CovidJsonRecord, S3ObjectProcessor}
+import org.codecraftlabs.aries.util.{CovidJsonRecord, CovidRecord, S3ObjectProcessor}
 import org.codecraftlabs.aries.util.SQSUtil.{deleteMessages, getRecords}
+import org.json4s.{DefaultFormats, Formats}
+import org.json4s.jackson.Serialization.read
 
 import scala.util.Properties.envOrElse
 
 class CovidRecordExportLambda {
+  private val dateTimeFormatYYYY_MM_DD = new SimpleDateFormat("yyyy/MM/dd")
   private val logger: Logger = LogManager.getLogger(getClass)
 
   def processRecords(scheduleEvent: ScheduledEvent): String = {
@@ -23,9 +28,12 @@ class CovidRecordExportLambda {
   }
 
   private def processRecord(entry: CovidJsonRecord): String = {
+    implicit lazy val jsonFormats: Formats = DefaultFormats
     val bucket = envOrElse(DestinationS3Bucket, "")
     val prefix = envOrElse(DestinationS3Prefix, "")
-    val keyName = prefix + "/" + entry.messageId + ".json"
+    val convertedJson = read[CovidRecord](entry.contents)
+    val convertedDateTime = dateTimeFormatYYYY_MM_DD.format(convertedJson.lastUpdate)
+    val keyName = prefix + "/" + convertedDateTime + "/" + entry.messageId + ".json"
     S3ObjectProcessor.writeObject(bucket, keyName, entry.contents)
     entry.receiptHandle
   }
