b0cd24ebfcd38b41e9146b4eccf4a4592fef40af
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-08-22 17:48:32 +0200

    more code

--- src/main/scala/org/codecraftlabs/aries/AthenaPartitionUpdateLambda.scala
+++ src/main/scala/org/codecraftlabs/aries/AthenaPartitionUpdateLambda.scala
@@ -10,14 +10,13 @@ import scala.util.Properties
 class AthenaPartitionUpdateLambda {
   private val logger: Logger = LogManager.getLogger(getClass)
 
-  def handleRequest(scheduleEvent: ScheduledEvent): String = {
+  def handleRequest(scheduleEvent: ScheduledEvent): Unit = {
     logger.info("Starting Athena partition update lambda")
     val dateTime = scheduleEvent.getTime
     val year = dateTime.getYear.toString
     val month = dateTime.toString("MM")
     val day = dateTime.toString("dd")
     processPartition(year, month, day)
-    "Done"
   }
 
   private def processPartition(year: String, month: String, day: String): Unit = {
@@ -28,7 +27,7 @@ class AthenaPartitionUpdateLambda {
     if (covidDatabase.isDefined && covidTable.isDefined) {
       val s3Bucket = Properties.envOrElse(DestinationS3Bucket, "")
       val s3Prefix = Properties.envOrElse(DestinationS3Prefix, "")
-      val statement = "ALTER TABLE `" + covidDatabase.get + "`.`" + covidTable.get + "` ADD IF NOT EXISTS PARTITION (year = '" + year + ", month = '" + month + "', day ='" + day +  "') LOCATION 's3://" + s3Bucket + "/" + s3Prefix + "/" + path + "'"
+      val statement = "ALTER TABLE " + covidDatabase.get + "." + covidTable.get + " ADD IF NOT EXISTS PARTITION (year='" + year + "', month='" + month + "', day='" + day + "') LOCATION 's3://" + s3Bucket + "/" + s3Prefix + "/" + path + "'"
       executeQuery(statement)
     }
   }
